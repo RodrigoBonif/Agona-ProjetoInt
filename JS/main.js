1423b207cfb5d6e5a8f9693708055204e9747fe2
RodrigoBonif <digoboni22@gmail.com>
2021-11-15 11:00:27 -0300

    Nav-bar
    topbar quase finalizada

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+var btnContact = document.querySelector('.perfil');
+
+btnContact.addEventListener('click', function () {
+    var boxContact = document.querySelector('.box-perfil');
+    boxContact.classList.toggle('open-display');
+});
